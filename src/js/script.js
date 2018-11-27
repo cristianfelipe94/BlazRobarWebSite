1b374f637816086316fc99c00b9284234292bb68
Cristian Felipe <cristianpipe0494@gmail.com>
2018-11-27 10:48:32 -0600

    Have added the Basic HTML structure.

--- src/js/script.js
+++ src/js/script.js
@@ -2,9 +2,8 @@
  * Project Description.
  * ...
  */
-
-const text = 'Hola mundo!';
+/* const text = 'Hola mundo!';
 const container = document.getElementById('container');
 const paragraph = document.createElement('p');
 paragraph.innerText = text;
-container.appendChild(paragraph);
+container.appendChild(paragraph); */
